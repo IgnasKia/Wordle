47a93d00e55311025a57d06dccd7b310d92b9127
Ignas Kiaulakys <ignas.kiaulakys@gmail.com>
2022-02-28 18:00:17 +0200

    Used letters have different background color in keyboard

--- src/components/Keyboard/KeyboardPress.js
+++ src/components/Keyboard/KeyboardPress.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./keyboardPress.css";
 
-function KeyboardPress({ keyPressed }) {
+function KeyboardPress({
+  keyPressed,
+  greenKey = [],
+  greyKey = [],
+  yellowKey = [],
+}) {
   const keys = [
     "q",
     "w",
@@ -33,11 +38,30 @@ function KeyboardPress({ keyPressed }) {
     "ENTER",
   ];
 
+  function keyBackgroundColor(key) {
+    if (greenKey.includes(key)) {
+      return "green";
+    }
+    if (greyKey.includes(key)) {
+      return "grey";
+    }
+    if (yellowKey.includes(key)) {
+      return "darkOrange";
+    }
+    return "darkgrey";
+  }
+
   return (
     <div className="row-buttons">
       {keys.map((key) => {
         return (
-          <button key={key} onClick={() => keyPressed(key)}>
+          <button
+            key={key}
+            onClick={() => keyPressed(key)}
+            style={{
+              backgroundColor: keyBackgroundColor(key),
+            }}
+          >
             {key.toUpperCase()}
           </button>
         );
